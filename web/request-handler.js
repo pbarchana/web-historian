b8360d8e358356c7905a1d4316f5a27f8f161b14
Hack Reactor Students <archana.pb@gmail.com>
2013-12-09 15:02:47 -0800

    Accepts GET request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,10 @@
 var path = require('path');
+var helpers = require('./http-helpers.js');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
+
 
 module.exports.handleRequest = function (req, res) {
   console.log(exports.datadir);
+  res.writeHead(200, helpers.headers);
+  res.end('<input></input>');
 };
